7a38a32c12f989634d325d5954c406960bc56678
AndrewFedorchenko <fedorchenko.andrey29071995@gmail.com>
2022-08-31 23:01:48 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './sass/main.scss';
+
+import './scripts/customer/slider.js';
+
+import './scripts/contacts-btn.js';
+import './scripts/mobile.js';
+import './scripts/btn-back';
+import './scripts/products-mod'
+
+import './scripts/products-mod2'
+import './scripts/products-mod3'
+
+import './scripts/modal-about.js'
